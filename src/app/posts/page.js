45c1cf83cb15a972095b78de1fe7eb9e99de4ae7
MollikaComputer Provider <mollikacomputer3@gmail.com>
2024-02-23 06:50:12 +0600

    fetch data from db implement daisy card

---
+++ src/app/posts/page.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const PostPage = async() => {
+    const res = await fetch("http://localhost:5000/posts");
+    const posts = await res.json();
+    return (
+        <div className=' text-center w-full'>
+
+                <h1 className='text-4xl'> Show All Posts</h1>
+                <h2 className='text-2xl'> Total Posts : {posts.length}</h2>
+                {
+                    posts.map((post)=>(
+                        <div key={post.id} className="card bg-gray-100 shadow-xl my-4 w-[70%] mx-auto">
+                            <div className="card-body">
+                                <h2 className="card-title">{post.title}</h2>
+                                <p>{post.description}</p>
+                                <p> Likes: {post.like_counts}</p>
+                                <div className="card-actions justify-end">
+                                <button className="btn btn-primary">See More</button>
+                                </div>
+                            </div>
+                            </div>
+                    ))
+                }
+
+        </div>
+    );
+};
+
+export default PostPage;
