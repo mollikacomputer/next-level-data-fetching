41225a0fb46d8a188f2cb2ddc766b4187b0aa509
MollikaComputer Provider <mollikacomputer3@gmail.com>
2024-02-28 07:19:25 +0600

    app route backend updated

--- src/app/api/blogs/route.js
+++ src/app/api/blogs/route.js
@@ -1,7 +1,19 @@
 import { NextResponse } from "next/server"
 
-
-
+// get api
 export const GET = () =>{
     return NextResponse.json({message:"Hello world from blog api route"});
-}
+};
+
+
+//post api
+export const POST = ()=>{};
+
+//patch api
+export const PATCH =()=>{};
+
+// put api
+export const PUT = ()=>{};
+
+// delete api
+export const DELETE = ()=>{};
